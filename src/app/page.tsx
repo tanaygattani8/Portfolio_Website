12bddc825bac4c689bdbddfdf07be4f7a69fb240
Tanay Gattani <tanaygattani8@gmail.com>
2026-04-08 18:34:13 -0500

    2026-04-08

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/organisms/Navbar";
 import HeroSection from "@/components/organisms/HeroSection";
 import SummarySection from "@/components/organisms/SummarySection";
 import ExperienceSection from "@/components/organisms/ExperienceSection";
-import SkillsSection from "@/components/organisms/SkillsSection";
 import CertificationsSection from "@/components/organisms/CertificationsSection";
 import ProjectsSection from "@/components/organisms/ProjectsSection";
 import ContactSection from "@/components/organisms/ContactSection";
